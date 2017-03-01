6efc0b2026a67abac99209768ec8ca55a817b6a3
Petros G. Sideris <pgsideris@gmail.com>
2017-03-01 22:04:34 +0000

    update conf for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,7 @@ import os
 import django
 from django.conf import settings
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_request.settings')
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
-    }
-}
 
 
 def pytest_configure():
